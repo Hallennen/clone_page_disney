2422545e4b6056f8318e37b3f9d4a31d869fe648
Hallennen Marinho <64508688+Hallennen@users.noreply.github.com>
2024-01-04 12:52:49 -0300

    ATT 04/01
    ETAPA FUNCIONAL E TELA DE SENHA

--- page_disney.py
+++ page_disney.py
@@ -1,10 +1,34 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
+from conexao_bd import conecta_no_banco
+
 
 page = Flask(__name__)
 
-@page.route("/")
+@page.route("/", methods=['GET'])
 def teste():
+
     return render_template('disney.html')
+
+@page.route("/passo2", methods=['POST'])
+def validar():
+    email_formulario = request.form['email_user']
+
+    
+    try:
+        busca = conecta_no_banco.connect_banco('SELECT email FROM logins',opção='fetchall')
+        for item in busca:
+            print(item[0])
+            if email_formulario == item[0]:
+                print(True)
+                retorno = render_template('disney_passo2.html', email = email_formulario)
+                break
+            else:
+                retorno = render_template('disney_error.html', email= email_formulario)
+
+        return retorno
+                
+    except:
+        print()
 
 
 page.run(host='localhost',port=5000 , debug=True)
