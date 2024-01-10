ad4e4958bf632127435cc0846a929368372554a9
Hallennen Marinho <64508688+Hallennen@users.noreply.github.com>
2024-01-10 08:41:43 -0300

    Update page_disney.py
    correção script 10/01

--- page_disney.py
+++ page_disney.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from flask import Flask , render_template , request
 from conexao_bd import conecta_no_banco
 
@@ -77,53 +77,3 @@ page.run(host='localhost',port=5000 , debug=True)
 
 
 
-
-
-
-
-
-
-# class cadastro_de_user():
-#     def cadastro_email(self):
-
-#         return render_template('cadastro_email.html')
-
-#     def cadastro_no_banco(self):
-
-#         return render_template('disney.html')
-=======
-from flask import Flask , render_template , request
-from conexao_bd import conecta_no_banco
-
-
-page = Flask(__name__)
-
-@page.route("/", methods=['GET'])
-def teste():
-
-    return render_template('disney.html')
-
-@page.route("/passo2", methods=['POST'])
-def validar():
-    email_formulario = request.form['email_user']
-
-    
-    try:
-        busca = conecta_no_banco.connect_banco('SELECT email FROM logins',opção='fetchall')
-        for item in busca:
-            print(item[0])
-            if email_formulario == item[0]:
-                print(True)
-                retorno = render_template('disney_passo2.html', email = email_formulario)
-                break
-            else:
-                retorno = render_template('disney_error.html', email= email_formulario)
-
-        return retorno
-                
-    except:
-        print()
-
-
-page.run(host='localhost',port=5000 , debug=True)
->>>>>>> b2eae3bbf5c099914b956695efd63796f23d2b3a
