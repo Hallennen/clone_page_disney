d9c4fa748d387144fb045823ee06688bfa5ad8b5
Hallennen Marinho <64508688+Hallennen@users.noreply.github.com>
2024-01-15 09:55:09 -0300

    ATT 15/01
    IMPLANTAÇÃO INSERÇÃO POR INCREMENTAL NO BANCO, E VALIDAÇÃO EFETIVA DE SENHA DE LOGIN

--- page_disney.py
+++ page_disney.py
@@ -1,10 +1,12 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> fdbaa55 (IMPLEMENTAÇÃO INSERÇÃO INCREMENTAL E VALIDA SENHA 15/01)
 from flask import Flask , render_template , request
 from conexao_bd import conecta_no_banco
 
-
 page = Flask(__name__)
-
+    
 @page.route("/", methods=['GET'])
 def teste():
 
@@ -35,16 +37,19 @@ def validar():
 
 @page.route("/main", methods=['POST'])
 def tela_principal():
+    senha_email = request.form['senha_email_user']
+    busca = conecta_no_banco.connect_banco(f'''SELECT senha FROM logins WHERE email = '{email_formulario}' ''',opção='fetchone')
 
-
-    return render_template('main.html', email = email_formulario)
+    if senha_email == busca[0]:
+        return render_template('main.html', email = email_formulario)
+    else:
+        return ('<H1>Senha incorreta</H1>')
 
 
 @page.route("/cadastro", methods=['POST'])
 def cadastro_email():
     global senha_user_email
     senha_user_email = request.form['senha_email_novo_user']
-    # conecta_no_banco.connect_banco('INSERT ITO ')
 
     return render_template('cadastro_email.html')
 
@@ -62,10 +67,10 @@ def cadastro():
     telefone_arrumado = telefone[:2]  + ' '  + telefone[2:7] + '-' + telefone[7:]
     permissao = 'padrão'
 
-    # print(nome, idade, cidade, rua, cpf, telefone, email_formulario, senha_user_email, permissao )
-    print('deu certo, passou no teste1')
-    conecta_no_banco.connect_banco(f'''INSERT INTO logins (user_id,nome,idade,cidade,rua,cpf,telefone,email,senha,permissao)
-                                        VALUES ('22','{nome}','{idade}','{cidade}', '{rua}', '{cpf_arrumado}', '{telefone_arrumado}','{email_formulario}', '{senha_user_email}', '{permissao}') ''', 'insert')
+
+
+    conecta_no_banco.connect_banco(f'''INSERT INTO logins (nome,idade,cidade,rua,cpf,telefone,email,senha,permissao)
+                                        VALUES ('{nome}','{idade}','{cidade}', '{rua}', '{cpf_arrumado}', '{telefone_arrumado}','{email_formulario}', '{senha_user_email}', '{permissao}') ''', 'insert')
 
 
     return teste()
@@ -77,3 +82,20 @@ page.run(host='localhost',port=5000 , debug=True)
 
 
 
+<<<<<<< HEAD
+=======
+
+
+
+
+
+
+# class cadastro_de_user():
+#     def cadastro_email(self):
+
+#         return render_template('cadastro_email.html')
+
+#     def cadastro_no_banco(self):
+
+#         return render_template('disney.html')
+>>>>>>> fdbaa55 (IMPLEMENTAÇÃO INSERÇÃO INCREMENTAL E VALIDA SENHA 15/01)
